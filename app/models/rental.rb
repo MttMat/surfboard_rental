b01f38468e50463ea257fb8bbf51aab2a1ce0149
matteo vigolo <matteo.vig27@gmail.com>
2022-08-22 18:15:48 +0800

    Fixed private method in rental model

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -4,4 +4,14 @@ class Rental < ApplicationRecord
   validates :starting_date, :return_date, presence: true, availability: true
   validate :return_date_after_starting_date
   validates :booking_status, presence: true
+
+  private
+
+  def return_date_after_starting_date
+    return if return_date.blank? || starting_date.blank?
+
+    if return_date < starting_date
+      errors.add(:return_date, "must be after the start date")
+    end
+ end
 end
