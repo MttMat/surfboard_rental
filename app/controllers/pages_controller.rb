9e462ba7b90b84083fc22bfa563649e3cc9b8c6b
Angger Prayugo <anggerprayugo251@gmail.com>
2022-08-24 14:35:46 +0800

    Homepage created

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @surfboards = Surfboard.all
+  end
+
+  def index
+    @surfboards = Surfboard.all
   end
 end
